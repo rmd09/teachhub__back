eca1e1db29b31857d7564d9681d052e2c2c8e680
rmd09 <rmd09@yandex.ru>
2024-07-11 23:52:38 +0300

    fix: очередной баг с авторизацией

--- middlewares/teacher.js
+++ middlewares/teacher.js
@@ -2,9 +2,14 @@ const { teachers } = require("../database/models")
 
 const getMe = async(req, res, next) => {
     const user = await teachers.findById(req.jwtObject._id);
-    user.password = "";
-    req.user = user;
-    next();
+    if (user) {
+        user.password = "";
+        req.user = user;
+        next();
+    }
+    else {
+        res.status(401).send("Unauthorized");
+    }
 }
 
 module.exports = {
